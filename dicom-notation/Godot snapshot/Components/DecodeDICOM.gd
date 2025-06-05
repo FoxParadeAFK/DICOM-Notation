07be6dd62ecad97d37eb6fc32047d6020056b261
FoxParadeAFK <s.kittwong004@gmail.com>
2025-06-05 15:42:19 +0100

    Complete parsing of mri images to project

--- dicom-notation/Godot snapshot/Components/DecodeDICOM.gd
+++ dicom-notation/Godot snapshot/Components/DecodeDICOM.gd
@@ -4,7 +4,7 @@ var valueInformation : Dictionary[String, Variant]
 @export var sprite3D : Sprite3D
 
 func _ready() -> void:
-	var reader : FileAccess = FileAccess.open("res://1.000000-T2Post1-61265/1-01.dcm", FileAccess.READ)
+	var reader : FileAccess = FileAccess.open("res://1.000000-T2Post1-61265/1-18.dcm", FileAccess.READ)
 	var decoder : DecodeElement = DecodeElement.new(reader, valueInformation)
 
 	reader.seek(128) # skip "file preamble" 128 bytes
@@ -19,20 +19,20 @@ func _ready() -> void:
 	while reader.get_position() < reader.get_length(): decoder.ReadElement() # file data
 	
 	#TODO - clean this up after we figure out how to render the pixel data
-	# var fileData : Dictionary[String, Variant] = valueInformation.duplicate(true)
-	# fileData.erase("Pixel Data")
+	var fileData : Dictionary[String, Variant] = valueInformation.duplicate(true)
+	fileData.erase("Pixel Data")
 
-	# var json = FileAccess.open("res://Godot snapshot/Parsed Data/1-01.json", FileAccess.WRITE)
-	# json.store_string(JSON.stringify(fileData, "\t", false))
-	# json.close()
+	var json = FileAccess.open("res://Godot snapshot/Parsed Data/1-18.json", FileAccess.WRITE)
+	json.store_string(JSON.stringify(fileData, "\t", false))
+	json.close()
 
-	var raw = FileAccess.open("res://Godot snapshot/Parsed Data/1-01.raw", FileAccess.WRITE)
+	var raw = FileAccess.open("res://Godot snapshot/Parsed Data/1-18.raw", FileAccess.WRITE)
 	for word in valueInformation["Pixel Data"]:
 		raw.store_16(word)
 	
 	raw.close()
 
-	var imageData : FileAccess = FileAccess.open("res://Godot snapshot/Parsed Data/1-01.raw", FileAccess.READ)
+	var imageData : FileAccess = FileAccess.open("res://Godot snapshot/Parsed Data/1-18.raw", FileAccess.READ)
 	var byteData : PackedByteArray
 	while imageData.get_position() < imageData.get_length():
 		var word : int = imageData.get_16()
@@ -40,4 +40,4 @@ func _ready() -> void:
 		byteData.append(byte)
 
 	var mriImage : Image = Image.create_from_data(512, 512, false, Image.FORMAT_L8, byteData)
-	mriImage.save_png("res://Godot snapshot/Parsed Data/1-01.png")
+	mriImage.save_png("res://Godot snapshot/Parsed Data/1-18.png")

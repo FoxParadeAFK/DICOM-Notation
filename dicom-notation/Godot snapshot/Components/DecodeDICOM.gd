6e0846f1b6b0b6c175df00c1496a04a1f22fc137
FoxParadeAFK <s.kittwong004@gmail.com>
2025-06-15 23:42:25 +0100

    Refined folder searcher + multi-parser + ditched json storage

--- dicom-notation/Godot snapshot/Components/DecodeDICOM.gd
+++ dicom-notation/Godot snapshot/Components/DecodeDICOM.gd
@@ -1,43 +1,49 @@
 extends Node3D
 
-var valueInformation : Dictionary[String, Variant]
-@export var sprite3D : Sprite3D
+const folder : String = "1.000000-T2Post1-61265"
+const relativeDirectory : String = "res://%s" % folder
+const rawPath : String = "res://Parsed/%s/Raw" % folder
+const pngPath : String = "res://Parsed/%s/PNG" % folder
 
-func _ready() -> void:
-	var reader : FileAccess = FileAccess.open("res://1.000000-T2Post1-61265/1-18.dcm", FileAccess.READ)
+func ReadDirectory(path : String) -> PackedStringArray:
+	var directory : DirAccess = DirAccess.open(path)
+	if not directory: return PackedStringArray()
+
+	return directory.get_files()
+
+func ReadDICOM(file : String) -> void:
+	var valueInformation : Dictionary[String, Variant]
+	var reader : FileAccess = FileAccess.open("%s/%s" % [relativeDirectory, file], FileAccess.READ)
 	var decoder : DecodeElement = DecodeElement.new(reader, valueInformation)
 
 	reader.seek(128) # skip "file preamble" 128 bytes
-	print(reader.get_buffer(4).get_string_from_ascii()) # "dicom prefix" should spell "DICM" 4 bytes
-	print(decoder.ReadElement()) # "file meta information group length"
+	reader.get_buffer(4).get_string_from_ascii() # "dicom prefix" should spell "DICM" 4 bytes
+	decoder.ReadElement() # "file meta information group length"
 
 	var metaInformationLength : int = valueInformation.get("File Meta Information Group Length") + reader.get_position()
-	while reader.get_position() < metaInformationLength: print(decoder.ReadElement()) # meta information
-
+	while reader.get_position() < metaInformationLength: decoder.ReadElement() # meta information
 	decoder.DeduceTransferSyntax(valueInformation.get("Transfer Syntax UID"))
 
 	while reader.get_position() < reader.get_length(): decoder.ReadElement() # file data
-	
-	#TODO - clean this up after we figure out how to render the pixel data
-	var fileData : Dictionary[String, Variant] = valueInformation.duplicate(true)
-	fileData.erase("Pixel Data")
 
-	var json = FileAccess.open("res://Godot snapshot/Parsed Data/1-18.json", FileAccess.WRITE)
-	json.store_string(JSON.stringify(fileData, "\t", false))
-	json.close()
+	SaveRaw(file, valueInformation)
+	RenderImage(file, valueInformation)
 
-	var raw = FileAccess.open("res://Godot snapshot/Parsed Data/1-18.raw", FileAccess.WRITE)
-	for word in valueInformation["Pixel Data"]:
-		raw.store_16(word)
+func RenderImage(file : String, valueInformation : Dictionary[String, Variant]) -> void:
+	var pixelData : FileAccess = FileAccess.open("%s/%s" % [rawPath, file.replace(".dcm", ".raw")], FileAccess.READ)
+	var bytes : PackedByteArray
+	while pixelData.get_position() < pixelData.get_length():
+		var byte : int = round(pixelData.get_16() / (valueInformation["Largest Image Pixel Value"][0] / 256))
+		bytes.append(byte)
+
+	Image.create_from_data(512, 512, false, Image.FORMAT_L8, bytes).save_png("%s/%s" % [pngPath, file.replace(".dcm", ".png")])
+
+func SaveRaw(file : String, valueInformation : Dictionary[String, Variant]) -> void:
+	var raw : FileAccess = FileAccess.open("%s/%s" % [rawPath, file.replace(".dcm", ".raw")], FileAccess.WRITE)
+	for word in valueInformation["Pixel Data"]: raw.store_16(word)
 	
 	raw.close()
 
-	var imageData : FileAccess = FileAccess.open("res://Godot snapshot/Parsed Data/1-18.raw", FileAccess.READ)
-	var byteData : PackedByteArray
-	while imageData.get_position() < imageData.get_length():
-		var word : int = imageData.get_16()
-		var byte : int = round(word / 21.59375)
-		byteData.append(byte)
-
-	var mriImage : Image = Image.create_from_data(512, 512, false, Image.FORMAT_L8, byteData)
-	mriImage.save_png("res://Godot snapshot/Parsed Data/1-18.png")
+func _ready() -> void:
+	var content : PackedStringArray = ReadDirectory(relativeDirectory)
+	for file in content: ReadDICOM(file)
